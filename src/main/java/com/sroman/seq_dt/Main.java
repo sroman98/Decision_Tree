330f3bc307955b271aec3951a38cad361e684498
Sandra <sandraromanrivera@hotmail.com>
2021-05-14 10:02:11 -0500

    Cleaner printing

--- src/main/java/com/sroman/seq_dt/Main.java
+++ src/main/java/com/sroman/seq_dt/Main.java
@@ -4,44 +4,26 @@ import java.util.Map;
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
         final String[][] data = Helpers.getMatrixFromCSV("weather.csv");
         Dataset dataset = new Dataset(data);
-
-//        System.out.println("System entropy:");
-//        System.out.println(dataset.getEntropy());
-//        
-//        System.out.println("\nSubentropies:");
-//        for(Map.Entry subentropy : dataset.getSubentropies().entrySet()) {
-//            System.out.println(subentropy.getKey() + ": " + subentropy.getValue());
-//        }
-//        
-//        System.out.println("\nGains:");
-//        for(Map.Entry gain : dataset.getGains().entrySet()) {
-//            System.out.println(gain.getKey() + ": " + gain.getValue());
-//        }
-//        
-//        System.out.println("\nGreatest gain:");
-//        Map.Entry gg = dataset.getGreatestGain();
-//        System.out.println(gg.getKey() + ": " + gg.getValue());
         recursiveEntropy(dataset, 0);
     }
 
     static void recursiveEntropy(Dataset dataset, int iter) {
+        String tabs = "";
+                for(int i = 0; i < iter; i++)
+                    tabs += "\t\t";
+                
         if (iter == 3) {
-            System.out.println("DONE");
+            System.out.println(tabs + "DONE");
         } else if (dataset.getEntropy() == 0) {
-            System.out.println(dataset.getYValue());
+            System.out.println(tabs + dataset.getYValue());
         } else {
-            System.out.println("\nGreatest gain:");
-            Map.Entry gg = dataset.getGreatestGain();
-            System.out.println(gg.getKey() + ": " + gg.getValue());
             Attribute a = dataset.getGreatestGainAttribute();
+            System.out.println(tabs + a.getName() + "");
             for (Map.Entry<String, Dataset> e : a.getSubdatasets().entrySet()) {
-                System.out.println("\n" + e.getKey() + ":");
+                System.out.println(tabs + "\t" + e.getKey() + ":");
                 recursiveEntropy(e.getValue(), iter + 1);
             }
         }
